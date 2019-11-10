87e0116ac297a3f2f20a985fd06be050a7ef1ef6
danielle20-meet <40920346+danielle20-meet@users.noreply.github.com>
2019-11-10 19:20:50 +0000

    10/11/19

---
+++ Lab3/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, redirect, url_for, render_template
+from flask import session as login_session
+
+app = Flask(__name__)
+app.secret_key = "MY_SUPER_SECRET_KEY"
+
+
+##### Code here ######
+
+@app.route('/')
+def homepa():
+	return render_template("home.html")
+@app.route('/about')
+def about():
+	return render_template("about.html")
+@app.route('/store')
+def store():
+	return render_template("store.html")
+@app.route('/cart')
+def cart():
+	return render_template("cart.html")
+#####################
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
